bb8f05b025d96ebd7d3f4843d98f8f65f4fed4ff
mingyu cao <m.cao@surrey.ac.uk>
2026-03-25 17:43:43 +0800

    add

--- opencompass/opencompass/configs/models/dllm/llada_base_8b.py
+++ opencompass/opencompass/configs/models/dllm/llada_base_8b.py
@@ -4,7 +4,7 @@ models = [
     dict(
         type=LLaDABaseModel,
         abbr='llada-8b-base',
-        path='GSAI-ML/LLaDA-8B-Base',
+        path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',
         batch_size=1,
         run_cfg=dict(num_gpus=1),
     )
