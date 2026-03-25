55262b3c9008b636b66385b64b881a61c5b5d36d
mingyu cao <m.cao@surrey.ac.uk>
2026-03-26 01:42:14 +0800

    add

---
+++ opencompass/eval_greedy.block32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="soar_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1                # 请求2块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate soar
+
+cp ../generate.greedy.py ../generate.py
+python run.py llada_instruct_gen_humaneval_length256_block32.py -w outputs/llada_instruct_gen_humaneval_length256_block32_logits_greedy
+
+# cp ../generate.greedy.py ../generate.py
+# python run.py llada_instruct_gen_mbpp_length256_block32.py -w outputs/llada_instruct_gen_mbpp_length256_block32_confidence_greedy
+
+# cp ../generate.greedy.py ../generate.py
+# python run.py llada_instruct_gen_gsm8k_length256_block32.py -w outputs/llada_instruct_gen_gsm8k_length256_block32_confidence_greedy
