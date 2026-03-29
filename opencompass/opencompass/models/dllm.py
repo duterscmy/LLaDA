fe92ab3269b356ec0493910aa5c959312c16e753
mingyu cao <m.cao@surrey.ac.uk>
2026-03-29 10:18:02 +0800

    add

--- opencompass/opencompass/models/dllm.py
+++ opencompass/opencompass/models/dllm.py
@@ -187,10 +187,15 @@ class LLaDAModel(BaseModel):
 
     def _load_tokenizer(self, path: str, tokenizer_path: Optional[str],
                         tokenizer_kwargs: dict):
-        from transformers import AutoTokenizer
+        from transformers import AutoTokenizer, PreTrainedTokenizerFast
         tokenizer_kwargs["trust_remote_code"] = True
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_path if tokenizer_path else path, **tokenizer_kwargs)
+        load_path = tokenizer_path if tokenizer_path else path
+        try:
+            self.tokenizer = AutoTokenizer.from_pretrained(
+                load_path, **tokenizer_kwargs)
+        except KeyError:
+            self.tokenizer = PreTrainedTokenizerFast.from_pretrained(
+                load_path, **tokenizer_kwargs)
 
         # A patch for some models without pad_token_id
         if self.pad_token_id is not None:
